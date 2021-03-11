5a8faff08c0f0e1b956af928ec3ecd6a34da58e2
cod3rcarl <cod3rcarl@gmail.com>
2021-03-11 11:01:39 +0000

    links go to the correct place

--- src/Pages/ContactPage.js
+++ src/Pages/ContactPage.js
@@ -1,46 +1,45 @@
-import React from "react";
-import ContactItem from "../components/ContactItem";
-import phone from "../img/phone.svg";
-import email from "../img/emailme.svg";
-import Title from "../components/Title";
-import { faGithub, faTwitter, faLinkedin } from "@fortawesome/free-brands-svg-icons";
+import React from 'react';
+import ContactItem from '../components/ContactItem';
+import phone from '../img/phone.svg';
+import email from '../img/emailme.svg';
+import Title from '../components/Title';
+import { faGithub, faTwitter, faLinkedin } from '@fortawesome/free-brands-svg-icons';
 
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 function ContactPage() {
   return (
     <div>
-      <div className="title">
-        <Title title={"Contact Me"} span={"Contact Me"} />
+      <div className='title'>
+        <Title title={'Contact Me'} span={'Contact Me'} />
       </div>
-      <div className="ContactPage">
-        <div className="map-sect">
+      <div className='ContactPage'>
+        <div className='map-sect'>
           <iframe
-            src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d19409.07007518956!2d-1.87262095!3d52.549105350000005!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2suk!4v1614900295967!5m2!1sen!2suk"
-            width="600"
-            title="location"
-            height="450"
+            src='https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d19409.07007518956!2d-1.87262095!3d52.549105350000005!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2suk!4v1614900295967!5m2!1sen!2suk'
+            width='600'
+            title='location'
+            height='450'
             style={{ border: 0 }}
-            allowfullscreen=""
-            loading="lazy"
+            allowfullscreen=''
+            loading='lazy'
           ></iframe>
         </div>
-        <div className="contact-sect">
-          <ContactItem icon={phone} text1={"07711816633"} title={"Phone"} />
-          <ContactItem icon={email} text1={"cod3rcarl@gmail.com"} title={"Email"} />
+        <div className='contact-sect'>
+          <ContactItem icon={phone} text1={'07711816633'} title={'Phone'} />
+          <ContactItem icon={email} text1={'cod3rcarl@gmail.com'} title={'Email'} />
         </div>
       </div>
-      <div className="icons">
-        <Link to="https://www.linkedin.com/in/carlmichaelmcintosh" className="icon-holder">
-          <FontAwesomeIcon icon={faLinkedin} className="icon li" />
-        </Link>
-        <Link to="https://github.com/cod3rcarl" className="icon-holder">
-          <FontAwesomeIcon icon={faGithub} className="icon gh" />
-        </Link>
-        <Link to="https://twitter.com/cod3rcarl" className="icon-holder">
-          <FontAwesomeIcon icon={faTwitter} className="icon tw" />
-        </Link>
+      <div className='icons'>
+        <a href='https://www.linkedin.com/in/carlmichaelmcintosh' target='_blank' rel='noreferrer' className='icon-holder'>
+          <FontAwesomeIcon icon={faLinkedin} className='icon li' />
+        </a>
+        <a href='https://github.com/cod3rcarl' target='_blank' rel='noreferrer' className='icon-holder'>
+          <FontAwesomeIcon icon={faGithub} className='icon gh' />
+        </a>
+        <a href='https://twitter.com/cod3rcarl' target='_blank' rel='noreferrer' className='icon-holder'>
+          <FontAwesomeIcon icon={faTwitter} className='icon tw' />
+        </a>
       </div>
     </div>
   );
--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -1,42 +1,42 @@
-import React from "react";
-import { faGithub, faTwitter, faLinkedin } from "@fortawesome/free-brands-svg-icons";
+import React from 'react';
+import { faGithub, faTwitter, faLinkedin } from '@fortawesome/free-brands-svg-icons';
 
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { Link } from 'react-router-dom';
 
 const HomePage = () => {
   return (
-    <div className="HomePage">
-      <header className="hero">
-        <h1 className="hero-text">
+    <div className='HomePage'>
+      <header className='hero'>
+        <h1 className='hero-text'>
           Hi, I'm <span>Carl</span>
         </h1>
         <h2>A Web Developer</h2>
-        <p className="h-sub-text"> 'My aim as a Web Developer is to be at least 1% better than I was Yesterday!'</p>
-        <div className="buttons ">
-          <Link to="/about">
-            <button className=" btn-port">About Me</button>
+        <p className='h-sub-text'> 'My aim as a Web Developer is to be at least 1% better than I was Yesterday!'</p>
+        <div className='buttons '>
+          <Link to='/about'>
+            <button className=' btn-port'>About Me</button>
           </Link>
-          <Link to="/portfolio">
-            <button className=" btn-port">View My Work</button>
+          <Link to='/portfolio'>
+            <button className=' btn-port'>View My Work</button>
           </Link>
-          <Link to="/blogs">
-            <button className=" btn-port">Read my Blogs</button>
+          <Link to='/blogs'>
+            <button className=' btn-port'>Read my Blogs</button>
           </Link>
-          <Link to="/contact">
-            <button className=" btn-port">Contact Me</button>
+          <Link to='/contact'>
+            <button className=' btn-port'>Contact Me</button>
           </Link>
         </div>
-        <div className="icons">
-          <Link to="https://www.linkedin.com/in/carlmichaelmcintosh" className="icon-holder">
-            <FontAwesomeIcon icon={faLinkedin} className="icon li" />
-          </Link>
-          <Link to="https://github.com/cod3rcarl" className="icon-holder">
-            <FontAwesomeIcon icon={faGithub} className="icon gh" />
-          </Link>
-          <Link to="https://twitter.com/cod3rcarl" className="icon-holder">
-            <FontAwesomeIcon icon={faTwitter} className="icon tw" />
-          </Link>
+        <div className='icons'>
+          <a href='https://www.linkedin.com/in/carlmichaelmcintosh' target='_blank' rel='noreferrer' className='icon-holder'>
+            <FontAwesomeIcon icon={faLinkedin} className='icon li' />
+          </a>
+          <a href='https://github.com/cod3rcarl' target='_blank' rel='noreferrer' className='icon-holder'>
+            <FontAwesomeIcon icon={faGithub} className='icon gh' />
+          </a>
+          <a href='https://twitter.com/cod3rcarl' target='_blank' rel='noreferrer' className='icon-holder'>
+            <FontAwesomeIcon icon={faTwitter} className='icon tw' />
+          </a>
         </div>
       </header>
     </div>
